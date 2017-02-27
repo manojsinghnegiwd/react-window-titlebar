ed74af9f696237c4042537f4c63059e105bc3f91
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-27 23:10:24 +0530

    added title prop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import style from './style';
 
 export default class TitleBar extends React.Component {
 	render () {
-		return <div></div>
+		const {title} = this.props;
+		return (
+			<div style={style.titleBar}>
+				<div></div>
+				<div>{title}</div>
+				<div></div>
+			</div>
+		)
 	}
 }
