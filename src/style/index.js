c211bbb287f45115ed09e6805ccafccf133ace0a
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-27 23:11:01 +0530

    using flexbox with titleBar

--- src/style/index.js
+++ src/style/index.js
@@ -1,7 +1,16 @@
 const style = {
 	titleBar: {
-		width: 100%,
-		height: 24
+		width: "100%",
+		height: 30,
+		paddingTop: 4,
+		paddingBottom: 4,
+		paddingRight: 10,
+		paddingLeft: 10,
+		"-webkit-app-region": "drag",
+		backgroundColor: "rgba(230, 230, 230, 0.52)",
+		position: "fixed",
+		display: 'flex',
+		justifyContent: 'space-between'
 	}
 }
 
