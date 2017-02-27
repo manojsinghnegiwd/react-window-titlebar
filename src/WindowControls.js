56c2e6570883a60a1811739398c1449b6ca29bc9
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-28 00:47:50 +0530

    added window control functions

--- src/WindowControls.js
+++ src/WindowControls.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 class WindowControls extends React.Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	closeWindow = (remote) => {
+		remote.getCurrentWindow().close();
+	}
+
+	minimize = (remote) => {
+		remote.getCurrentWindow().minimize();
+	}
+
+	maximize = (remote) => {
+		let window = remote.getCurrentWindow();
+
+		if(window.isMaximized())
+			window.unmaximize();
+		else
+			window.maximize();
+	}
+
+
 	render () {
+		const {remote} = this.props;
 		return (
 			<ul className="windowControls">
-				<li className="windowControlsButtons closeButton"></li>
-				<li className="windowControlsButtons minButton"></li>
-				<li className="windowControlsButtons maxButton"></li>
+				<li onClick={ () => this.closeWindow(remote) } className="windowControlsButtons closeButton"></li>
+				<li onClick={ () => this.minimize(remote) } className="windowControlsButtons minButton"></li>
+				<li onClick={ () => this.maximize(remote) } className="windowControlsButtons maxButton"></li>
 			</ul>
 		)
 	}
