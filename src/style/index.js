ea02beb9f11f9e53da00c59381bd23b27249ae25
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-27 23:44:42 +0530

    added style for window control

--- src/style/index.js
+++ src/style/index.js
@@ -6,11 +6,34 @@ const style = {
 		paddingBottom: 4,
 		paddingRight: 10,
 		paddingLeft: 10,
-		"-webkit-app-region": "drag",
+		WebkitAppRegion: "drag",
 		backgroundColor: "rgba(230, 230, 230, 0.52)",
 		position: "fixed",
 		display: 'flex',
 		justifyContent: 'space-between'
+	},
+	windowControls: {
+		display: "inline",
+		listStyle: "none",
+		float: "left",
+		padding: 0,
+		margin: 0
+	},
+	windowControlsButtons: {
+		width: 12,
+		height: 12,
+		float: "left",
+		margin: 5,
+		borderRadius: "50%"
+	},
+	closeButton: {
+		backgroundColor: "#fe5e59",
+	},
+	minButton: {
+		backgroundColor: "#fec42f",
+	},
+	maxButton: {
+		backgroundColor: "#2bd648",
 	}
 }
 
